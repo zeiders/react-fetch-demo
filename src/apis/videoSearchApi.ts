eb86edb0c8495eb296474f9e359daed0a796fc2b
zeiders <zeiders@illinois.edu>
2023-08-25 16:45:43 -0500

    refactored fetch into its own api file.

---
+++ src/apis/videoSearchApi.ts
@@ -0,0 +1,25 @@
+export type SearchResult<T = unknown> = {
+  results: Array<T>;
+  meta: { type: string; q: string; count: number };
+};
+
+export type SearchMatch = {
+  media_type: string;
+  media_source: string;
+  media_id: string;
+  name: string;
+  description: string;
+  image: string;
+  preview_image: string;
+  tags: Array<string>;
+  url: string;
+};
+
+export const fetchSearchResults = async (
+  term: string
+): Promise<SearchResult<SearchMatch>> => {
+  const apiUrl = `http://list.ly/api/v4/search/video?q=${term}`;
+  const response = await fetch(apiUrl);
+  const data = await response.json();
+  return data;
+};
